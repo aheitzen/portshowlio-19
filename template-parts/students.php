c8c200d6c53d35a78626cdbd55d78091464f23c2
sean01eary <oleary.sean.c@gmail.com>
2019-04-16 12:23:24 -0700

    typefaces per april 16

--- template-parts/students.php
+++ template-parts/students.php
@@ -1,1 +1,10 @@
 <div id="students"><h1>STUDENT GRID</h1></div>
+
+<h1>This is h1</h1>
+<h2>This is h2</h2>
+<h3>This is h3</h3>
+<h4>This is h4</h4>
+<h5>This is h5</h5>
+<h6>This is h6</h6>
+<p class="caption">This is caption</p>
+<p>this is p</p>
