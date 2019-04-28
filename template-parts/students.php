5f2ae902feec8ab55c8561ffed84f920ed55534b
sean01eary <oleary.sean.c@gmail.com>
2019-04-27 22:45:10 -0700

    grid links to students.php

--- template-parts/students.php
+++ template-parts/students.php
@@ -15,8 +15,10 @@
         <?php while ($query->have_posts()) : $query->the_post(); ?>
 
         <div class="students-test">
-            <h2 class="students-names"><?php echo get_the_title( $post_id ); ?></h2>
-            <img class="students-images" src="<?php the_field('headshot'); ?>" />
+            <a href="<?php the_permalink();?>">
+                <h2 class="students-names"><?php echo get_the_title( $post_id ); ?></h2>
+                <img class="students-images" src="<?php the_field('headshot'); ?>" />
+            </a>
             <p class="students-focus subhead"><?php the_field('focus'); ?></p>
             <div class="black-bar-long"></div>
         </div>
@@ -26,7 +28,7 @@
         <?php endwhile; ?>
     </div>
 
-    
+
     <h1 class="text-large">This is large</h1>
     <h1>This is h1</h1>
     <h2>This is h2</h2>
