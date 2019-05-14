00c3158f8e749789afe8884da0db59d5bf71c464
Alexa Heitzenrader <aheitzen1@gmail.com>
2019-05-13 22:31:11 -0700

    student post page

--- single-student.php
+++ single-student.php
@@ -5,17 +5,21 @@
 		<img class="single-post-headshot" src="<?php the_field('headshot'); ?>" />
 		<div class="secondary">
 		<!-- NAME -->
-		<h2><?php the_title(); ?></h2>
+		<h2 class="single-post-h2"><?php the_title(); ?></h2>
 		<!-- WEBSITE LINK -->
-		<a class='siteLink' href="http://<?php the_field('portfolio_site'); ?>" target='_blank'><?php the_field('portfolio_site'); ?></a>
+		<h3><a class='siteLink' href="http://<?php the_field('portfolio_site'); ?>" target='_blank'><?php the_field('portfolio_site'); ?></a></h3>
 		<!-- SOCIAL MEDIA -->
+		
 		<i class="fab fa-instagram"></i>
+		<i class="fab fa-facebook"></i>
+		<i class="fab fa-linkedin"></i>
+
 		<!-- THREE CATEGORIES OF FOCUS -->
 		<div class="three-categories-focus">
 		<?php $focus = get_field('focus');
 				if( $focus ): ?>
 				<?php foreach( $focus as $focus ): ?>
-						<span class='focus'><?php echo $focus; ?></span>
+						<p class='focus'><em><?php echo $focus; ?></em></p>
 				<?php endforeach; ?>
 			<?php endif; ?>
 
@@ -27,7 +31,7 @@
 <?php if( have_rows('projects') ): ?>
 	<?php while ( have_rows('projects') ) : the_row(); ?>
 		<?php if( get_row_layout() == 'project' ): ?>
-
+		<div class="featured-project">
 			<h3><?php the_sub_field('project_title'); ?></h3>
 			<h3><?php the_sub_field('project_type'); ?></h3>
 			
@@ -41,11 +45,12 @@
 					<?php endforeach; ?>
 					<?php wp_reset_postdata(); ?>
 				<?php endif; ?>
-			
+		</div>	
+		<div class="project-images">
 			<?php if( have_rows('project_images') ): ?>
 				<?php while ( have_rows('project_images') ) : the_row(); ?>
 
-					<!--2 COLUMN VERTICAL PORTRAIT -->
+					<!--2 COLUMN VERTICAL LAYOUT -->
 					<?php if( get_row_layout() == 'two_vertical_images' ): ?>
 						<div class="rowProject grid-verticle-two">
 							<?php if( have_rows('images') ): ?>
@@ -58,12 +63,12 @@
 							<?php endif; ?>
 						</div>
 
-					<!--2 COLUMN PORTRAIT -->
-				    <?php elseif( get_row_layout() == 'portrait_2_column' ): ?>
-						<div class="rowProject grid-two">
+					<!--2 COLUMN HORIZONTAL LAYOUT -->
+				    <?php elseif( get_row_layout() == 'two_horizontal_images' ): ?>
+						<div class="rowProject grid-horizontal-two">
 				        	<?php if(get_sub_field('images')): ?>
 								<?php while(has_sub_field('images')): ?>
-									<div class="imageHalf">
+									<div class="imageGrid">
 										<?php $imageurl = get_sub_field('image'); ?>
 										<img src="<?php echo $imageurl; ?>" />
 									</div>
@@ -71,16 +76,16 @@
 							<?php endif; ?>
 						</div>
 								
-					<!--1 COLUMN PORTRAIT -->
-					<?php elseif( get_row_layout() == 'portrait_1_column' ): ?>
-						<div class="rowProject">
+					<!--2 COLUMN HORIZONTAL AND VERTICAL LAYOUT -->
+					<?php elseif( get_row_layout() == 'horizontal_then_vertical' ): ?>
+						<div class="rowProject grid-horizontal-then-vertical">
 							<div class="">
 								<?php $imageurl = get_sub_field('image'); ?>
 								<img src="<?php echo $imageurl; ?>" />
 							</div>
 						</div>			
 
-					<!--Landscape -->
+					<!--2 COLUMN VERICAL AND HORIZONTAL LAYOUT -->
 				    <?php elseif( get_row_layout() == 'landscape_image' ): ?>
 						<div class="rowProject">
 				       		<div class="">
@@ -96,9 +101,18 @@
 				        		<?php the_sub_field('video'); ?>
 				   			</div>
 				   		</div>
+
+				   	<!--Video -->
+				    <?php elseif( get_row_layout() == 'video' ): ?>
+						<div class="rowProject">
+							<div class="video">
+				        		<?php the_sub_field('video'); ?>
+				   			</div>
+				   		</div>
 				   	<?php endif; ?>
 				<?php endwhile; ?>
 			<?php endif; ?>
+			</div>
 		<?php endif; ?>
 	<?php endwhile; ?>
 <?php endif; ?>
@@ -113,7 +127,6 @@
 
 		
 </div>
-
 
 
 
